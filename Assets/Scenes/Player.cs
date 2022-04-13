1c08334c36c56024fe634f12554df2bb1e49a6a6
Tino <tinopiaggi@gmail.com>
2022-04-13 17:28:31 +0200

    Algorithm to compute the selectable Tiles with BUGS

--- Assets/Scenes/Player.cs
+++ Assets/Scenes/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -6,7 +7,10 @@ public class Player : MonoBehaviour
     public GameObject tile;
     private ArrayList _tiles;
     private GameObject[] _tilesObj;
-    
+    private ArrayList _selectableTiles;
+    private int _sumValue;
+    public int _sumSelectedTiles;
+
     void Start()
     {
         _tiles = new ArrayList(9);
@@ -17,24 +21,76 @@ public class Player : MonoBehaviour
             Vector3 tilePos = new Vector3(tr.position.x + i * 3.3f, tr.position.y,
                 tr.position.z);
             _tiles.Add(i);
-            _tilesObj[i-1] = Instantiate(tile, tilePos, Quaternion.identity);
-            _tilesObj[i-1].GetComponent<Transform>().Rotate(45f,0f,0f);
-            _tilesObj[i-1].GetComponent<Tile>().SetTextNum(i);
-            _tilesObj[i-1].GetComponent<Tile>().setOwner(this);
+            _tilesObj[i - 1] = Instantiate(tile, tilePos, Quaternion.identity);
+            _tilesObj[i - 1].GetComponent<Transform>().Rotate(45f, 0f, 0f);
+            _tilesObj[i - 1].GetComponent<Tile>().SetTextNum(i);
+            _tilesObj[i - 1].GetComponent<Tile>().setOwner(this);
         }
+
+        _tiles.Remove(5);
+        _sumValue = 6;
+        _sumSelectedTiles = 0;
+        _selectableTiles = ComputeSelectable(_tiles);
     }
 
 
     public bool FlipTile(int number)
     {
-        if (_tiles.Contains(number))
+        if (_selectableTiles.Contains(number))
         {
             _tiles.Remove(number);
+            _selectableTiles = ComputeSelectable(_tiles);
+            _sumSelectedTiles += number;
             return true;
         }
-        
+
         return false;
     }
 
-    
+    private ArrayList ComputeSelectable(ArrayList arraySelectable)
+    {
+        ArrayList tmp = arraySelectable;
+        ArrayList array = new ArrayList();
+        for (int i = 0; i <= _sumValue; i++)
+        {
+            if (tmp.Contains(i))
+            {
+                //Double combination
+                int possibleCouple = (i + _sumSelectedTiles);
+                if (tmp.Contains(_sumValue - possibleCouple))
+                {
+                    if (!array.Contains(i)) array.Add(i);
+                    if (!array.Contains(_sumValue - possibleCouple)) array.Add(possibleCouple);
+                }
+                else if (possibleCouple == _sumValue)
+                {
+                    if (!array.Contains(i)) array.Add(i);
+                }
+
+                //Triple combination
+                for (int x = i + 1; x <= _sumValue; x++)
+                {
+                    if (tmp.Contains(x))
+                    {
+                        int possibleTriplet = (_sumSelectedTiles + i + x);
+                        if (tmp.Contains(_sumValue - possibleTriplet) && possibleTriplet != i && possibleTriplet != x)
+                        {
+                            if (!array.Contains(i)) array.Add(i);
+                            if (!array.Contains(x)) array.Add(x);
+                            if (!array.Contains(_sumValue - possibleTriplet)) array.Add(_sumValue - possibleTriplet);
+                        }
+                    }
+                }
+            }
+
+            
+            Debug.Log("ITERAZIONE NUMERO " + i);
+            foreach (int numero in array)
+            {
+                Debug.Log(numero);
+            }
+            
+        }
+        return array;
+    }
 }
