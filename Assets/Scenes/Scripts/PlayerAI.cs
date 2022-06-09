182dd68f2b6b8d346b42b32b106fec3ff5a1f18b
Martino <tinopiaggi@gmail.com>
2022-06-09 23:18:39 +0200

    fixed and refactored legalMoves, added method to compute sets

--- Assets/Scenes/Scripts/PlayerAI.cs
+++ Assets/Scenes/Scripts/PlayerAI.cs
@@ -7,29 +7,43 @@ public class PlayerAI : Player
 {
     private bool _computing;
     private bool _computed;
-    private ArrayList bestMove;
+    private Queue<int> bestMove;
+
+
+    private void Update()
+    {
+        if (!selectEnabled) return;
+        mcts oracle = new mcts();
+        StartCoroutine(oracle.computeBestMove(this,gameMan.maximumScore(),_selectableTiles, _sumDices,_sumSelectedTiles));
+        var tileToSelect = (int)_selectableTiles[0]; //at the moment select the first tile in the selectable tiles, that is computed by GameManager 
+        _tilesObj[tileToSelect-1].GetComponent<Tile>().Flip();
+    }
+
+    /*
     private void Update()
     {
         if (!selectEnabled) return; //selectEnabled is true when the Game Manager receive the result of the dices from the DicesLauncher 
         if (_computing) return;
-        if (_computed)
+        if (_computed) //when computed playerAI knows which are the best moves and play them 
         {
-            int tileToSelect = (int)bestMove[0];
-            _tilesObj[tileToSelect-1].GetComponent<Tile>().Flip(); 
+            int tileToSelect = bestMove.Dequeue();
+            _tilesObj[tileToSelect-1].GetComponent<Tile>().Flip();
+            if (bestMove.Count == 0) _computed = false; //end of turn
         }
         else
         {
             mcts oracle = new mcts();
-            StartCoroutine(oracle.computeBestMove(this,_selectableTiles, _remainingValue, gameMan.maximumScore()));
+            StartCoroutine(oracle.computeBestMove(this,gameMan.maximumScore(),_selectableTiles, _sumDices,_sumSelectedTiles));
             _computing = true;
         }
     }
+*/
 
-
-    public void takeAdvice(ArrayList bestMove)
+    public void takeAdvice(Queue<int> bestMove)
     {
         this.bestMove = bestMove;
         _computing = false;
         _computed = true;
     }
+    
 }
--- Assets/Scenes/Scripts/legalMoves.cs
+++ Assets/Scenes/Scripts/legalMoves.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class legalMoves : MonoBehaviour
+public static class legalMoves 
 {
     public static ArrayList compute(ArrayList currentTiles, int sumDices, int sumSelectedTiles)
     {
@@ -10,12 +10,9 @@ public class legalMoves : MonoBehaviour
         var array = new ArrayList();
 
         if (tmp.Contains(sumDices - sumSelectedTiles)) array.Add(sumDices - sumSelectedTiles);
-        for (int i = 1; i <= (sumDices - i); i++)
+        for (var i = 1; i <= (sumDices - i); i++)
         {
             if (!tmp.Contains(i)) continue;
-            if (i + sumSelectedTiles == sumDices)
-                if (!array.Contains(i))
-                    array.Add(i);
             for (var x = i + 1; x <= (sumDices - i); x++)
             {
                 if (!tmp.Contains(x)) continue;
@@ -29,14 +26,89 @@ public class legalMoves : MonoBehaviour
                     for (var y = x + 1; y <= (sumDices - y); y++)
                     {
                         if (!tmp.Contains(y)) continue;
-                        if (i + x + y + sumSelectedTiles != sumDices) continue;
-                        if (!array.Contains(x)) array.Add(x);
-                        if (!array.Contains(i)) array.Add(i);
-                        if (!array.Contains(y)) array.Add(y);
+                        if (i + x + y + sumSelectedTiles == sumDices)
+                        {
+                            if (!array.Contains(i)) array.Add(i);
+                            if (!array.Contains(x)) array.Add(x);
+                            if (!array.Contains(y)) array.Add(y);
+                        }
+                        else
+                        {
+                            for (var z = y + 1; z <= (sumDices - z); z++)
+                            {
+                                if (!tmp.Contains(z)) continue;
+                                if (i + x + y + z != sumDices) continue; //4 combinations in case of sumSelectedTiles=0; Very rare case. 
+                                if (!array.Contains(i)) array.Add(i);
+                                if (!array.Contains(x)) array.Add(x);
+                                if (!array.Contains(y)) array.Add(y);
+                                if (!array.Contains(z)) array.Add(z);
+                            }
+                        }
+                        
                     }
                 }
             }
         }
         return array;
     }
+    
+    public static ArrayList computeSets(ArrayList currentTiles, int sumDices)
+    {
+        var tmp = currentTiles;
+        var sets = new ArrayList();
+
+        if (tmp.Contains(sumDices))
+        {
+            HashSet<int> newSet = new HashSet<int>();
+            newSet.Add(sumDices);
+            sets.Add(newSet);
+        }
+        for (int i = 1; i <= (sumDices - i); i++)
+        {
+            if (!tmp.Contains(i)) continue;
+            for (var x = i + 1; x <= (sumDices - i); x++)
+            {
+                if (!tmp.Contains(x)) continue;
+                if (i + x == sumDices)
+                {
+                    HashSet<int> newSet = new HashSet<int>();
+                    newSet.Add(i);
+                    newSet.Add(x);
+                    sets.Add(newSet);
+                }
+                else
+                {
+                    for (var y = x + 1; y <= (sumDices - y); y++)
+                    {
+                        if (!tmp.Contains(y)) continue;
+                        if (i + x + y == sumDices)
+                        {
+                            HashSet<int> newSet = new HashSet<int>();
+                            newSet.Add(i);
+                            newSet.Add(x);
+                            newSet.Add(y);
+                            sets.Add(newSet);
+                        }
+                        else
+                        {
+                            for (var z = y + 1; z <= (sumDices - z); z++)
+                            {
+                                if (!tmp.Contains(z)) continue;
+                                if (i + x + y + z != sumDices) continue; //4 combinations in case of sumSelectedTiles=0; Very rare case. 
+                                HashSet<int> newSet = new HashSet<int>();
+                                newSet.Add(i);
+                                newSet.Add(x);
+                                newSet.Add(y);
+                                newSet.Add(z);
+                                sets.Add(newSet);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return sets;
+    }
+    
+    
 }
