6cd05552c2dd33d13e538b9e049af753695e8c2e
Martino <72280379+martinopiaggi@users.noreply.github.com>
2023-01-25 12:18:35 +0100

    inserted shell script

--- BSc(italian)/Foundations of Artificial Intelligence/buildPDF 1.sh
+++ BSc(italian)/Foundations of Artificial Intelligence/buildPDF 1.sh
@@ -1,2 +1,2 @@
-echo "Generating PDF for Economia e Organizzazione Aziendale"
-echo $(pandoc --resource-path=src:src/images src/*.md -o eco.pdf --verbose -f markdown-implicit_figures)
+echo "Generating PDF for Foundations of AI"
+echo $(pandoc --resource-path=src:src/images src/*.md -o fai.pdf -f markdown-implicit_figures)
