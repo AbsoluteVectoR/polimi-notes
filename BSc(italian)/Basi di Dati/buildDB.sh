45f27c47ce45b3eb7bae6870529828dcd131a103
Martino <72280379+martinopiaggi@users.noreply.github.com>
2023-04-02 21:17:03 +0200

    Basi di dati script fix name

--- BSc(italian)/Basi di Dati/buildDB.sh
+++ BSc(italian)/Basi di Dati/buildDB.sh
@@ -1,2 +1,2 @@
 echo "Generating PDF for Basi Di Dati"
-echo $(pandoc --resource-path=src:src/images src/*.md -o db2.pdf -f markdown-implicit_figures)
+echo $(pandoc --resource-path=src:src/images src/*.md -o basi_dati.pdf -f markdown-implicit_figures)
--- MSc(english) (WIP)/Databases 2/buildDB2.sh
+++ MSc(english) (WIP)/Databases 2/buildDB2.sh
@@ -1,2 +1,2 @@
 echo "Generating PDF for Databases 2"
-echo $(pandoc --resource-path=src:src/images src/*.md -o db2.pdf -f markdown-implicit_figures)
+echo $(pandoc --resource-path=src:src/images src/*.md -o databases2.pdf -f markdown-implicit_figures)
