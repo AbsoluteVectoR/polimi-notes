1e1db3e71279feb2cb826c3cac5c2d45ff8cab64
Martino <72280379+martinopiaggi@users.noreply.github.com>
2023-06-01 23:15:18 +0200

    [PPL] Erlang random exe

--- MSc(english) (WIP)/Principles of Programming Languages(WIP)/src/exe.erl
+++ MSc(english) (WIP)/Principles of Programming Languages(WIP)/src/exe.erl
@@ -1,5 +1,5 @@
--module(main).
--export([add/2,car/1,cdr/1,map/2]).
+%-module(main).
+%-export([add/2,car/1,cdr/1,map/2]).
 
 add(A,B) -> A + B.
 car([X|_]) -> X.
@@ -8,4 +8,40 @@ cdr([X|Xs]) -> Xs.
 map(_, []) -> [];
 map(F,[X|Xs]) -> [F(X) | map(F,Xs)].
 
+filterr(F, [H|T]) ->
+    case F(H) of
+        true  -> [H|filterr(F, T)];
+        false -> filterr(F, T)
+    end;
+filterr(F, []) -> [].
 
+%parallel versions
+
+
+% parallel map
+pmap(F, L) ->
+    Ps = [ spawn(?MODULE, execute, [F, X, self()]) || X <- L],
+    [receive 
+         {Pid, X} -> X
+     end || Pid <- Ps].
+
+execute(F, X, Pid) ->
+    Pid ! {self(), F(X)}.
+
+
+
+%same as pmap but we have to discard
+
+pfilter(F, L) ->
+    Ps = [ spawn(?MODULE, execute, [F, X, self()]) || X <- L],
+    lists:foldl(fun (P,Vo) ->
+               receive
+                  {P,true, X} -> Vo ++ [X];
+                  {P,false,_} -> Vo
+                end end, [], Ps).
+
+execute(F, X, Pid) ->
+    case F(X) of
+        true  -> Pid ! {self(), true,X};
+        false -> Pid ! {self(), false,X}
+    end.
